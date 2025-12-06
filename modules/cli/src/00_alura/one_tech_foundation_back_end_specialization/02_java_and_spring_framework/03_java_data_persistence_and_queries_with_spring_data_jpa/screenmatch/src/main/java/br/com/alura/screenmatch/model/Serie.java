c9b679abc8e6bf4a9045c363f1c316939adeac8c
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-12-06 19:56:37 -0300

    main: feat (screenmatch) - Updated Series model and added translation service
    - Updated the Series domain model and add a translation service plus DTOs to integrate external translation responses.

--- modules/cli/src/00_alura/one_tech_foundation_back_end_specialization/02_java_and_spring_framework/03_java_data_persistence_and_queries_with_spring_data_jpa/screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ modules/cli/src/00_alura/one_tech_foundation_back_end_specialization/02_java_and_spring_framework/03_java_data_persistence_and_queries_with_spring_data_jpa/screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,5 +1,7 @@
 package br.com.alura.screenmatch.model;
 
+import br.com.alura.screenmatch.service.traducao.ConsultaMyMemory;
+
 import java.util.OptionalDouble;
 
 public class Serie {
@@ -11,14 +13,14 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    public Serie(DadosSerie dadosSerie) {
+    public Serie(DadosSerie dadosSerie){
         this.titulo = dadosSerie.titulo();
         this.totalTemporadas = dadosSerie.totalTemporadas();
         this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0);
         this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
-        this.sinopse = dadosSerie.sinopse();
+        this.sinopse = ConsultaMyMemory.obterTraducao(dadosSerie.sinopse()).trim();
     }
 
     public String getTitulo() {

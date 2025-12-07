6f2a7805d9c018eb31a28596fb41ba27c76b94f2
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-12-06 21:21:15 -0300

    main: feat (screenmatch) - Added sample env and updated module wiring/config
    - Added a sample environment file and updated the Screenmatch submodule's build and runtime configuration.
    - Adjusted the application entrypoint and properties to improve startup wiring and configuration handling.

--- modules/cli/src/00_alura/one_tech_foundation_back_end_specialization/02_java_and_spring_framework/03_java_data_persistence_and_queries_with_spring_data_jpa/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ modules/cli/src/00_alura/one_tech_foundation_back_end_specialization/02_java_and_spring_framework/03_java_data_persistence_and_queries_with_spring_data_jpa/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.principal.Principal;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,11 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+
 		Principal principal = new Principal();
 		principal.exibeMenu();
 	}

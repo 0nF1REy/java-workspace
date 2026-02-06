6c37e922a1c0c7eb9f7a1019c98e725d8211b0e7
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-02-06 13:58:17 -0300

    main: feat (65_agendador_horarios/api) - Added AgendamentoController and enhance repository/service logic
    - Introduced the AgendamentoController to handle scheduling endpoints, and updated repository and service layers to support appointment creation, deletion, retrieval, and modification.

---
+++ modules/cli/src/65_agendador_horarios/src/main/java/dev/alanryan/agendadorhorarios/api/controller/AgendamentoController.java
@@ -0,0 +1,45 @@
+package dev.alanryan.agendadorhorarios.api.controller;
+
+import dev.alanryan.agendadorhorarios.api.infrastructure.entity.AgendamentoEntity;
+import dev.alanryan.agendadorhorarios.api.services.AgendamentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/agendamentos")
+@RequiredArgsConstructor
+public class AgendamentoController {
+
+    private final AgendamentoService agendamentoService;
+
+    @PostMapping
+    public ResponseEntity<AgendamentoEntity> salvarAgendamento(@RequestBody AgendamentoEntity agendamento) {
+        return ResponseEntity.accepted().body(agendamentoService.salvarAgendamento(agendamento));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarAgendamento(@RequestParam String cliente,
+                                                   @RequestParam LocalDateTime dataHoraAgendamento) {
+
+        agendamentoService.deletarAgendamento(dataHoraAgendamento, cliente);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AgendamentoEntity>> buscarAgendamentosDia(@RequestParam LocalDate data) {
+        return ResponseEntity.ok().body(agendamentoService.buscarAgendamentosDia(data));
+    }
+
+    @PutMapping
+    public ResponseEntity<AgendamentoEntity> alterarAgendamentos(@RequestBody AgendamentoEntity agendamento,
+                                                           @RequestParam String cliente,
+                                                           @RequestParam LocalDateTime dataHoraAgendamento) {
+        return ResponseEntity.accepted().body(agendamentoService.alterarAgendamento(agendamento,
+                cliente, dataHoraAgendamento));
+    }
+}
--- modules/cli/src/65_agendador_horarios/src/main/java/dev/alanryan/agendadorhorarios/api/infrastructure/repository/AgendamentoRepository.java
+++ modules/cli/src/65_agendador_horarios/src/main/java/dev/alanryan/agendadorhorarios/api/infrastructure/repository/AgendamentoRepository.java
@@ -1,4 +1,21 @@
 package dev.alanryan.agendadorhorarios.api.infrastructure.repository;
 
-public interface AgendamentoRepository {
+import dev.alanryan.agendadorhorarios.api.infrastructure.entity.AgendamentoEntity;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface AgendamentoRepository extends JpaRepository<AgendamentoEntity, Long> {
+
+    AgendamentoEntity findByServicoAndDataHoraAgendamentoBetween(String servico, LocalDateTime dataHoraInicio,
+                                                                 LocalDateTime dataHoraFinal);
+
+    @Transactional
+    void deleteByDataHoraAgendamentoAndCliente(LocalDateTime dataHoraAgendamento, String cliente);
+
+    List<AgendamentoEntity> findByDataHoraAgendamentoBetween(LocalDateTime dataHoraInicial, LocalDateTime dataHoraFinal);
+
+    AgendamentoEntity findByDataHoraAgendamentoAndCliente(LocalDateTime dataHoraAgendamento, String cliente);
 }
